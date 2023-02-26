1159acbcfad5d277da2c776b8befbe5a84b1945c
pczheng <zhengpc@geowit.cn>
2023-02-26 22:17:39 +0800

    暂存

--- vue3-vite/src/components/Relation.vue
+++ vue3-vite/src/components/Relation.vue
@@ -1,7 +1,7 @@
 <template>
     <a-row>
         <span>名称: </span>
-        <a-auto-complete style="width:200px" v-model:value="data.name" :options="nameOptions" />
+        <a-auto-complete style="width:200px" v-model:value="data.name" :options="nameOptions" @search="onSearch" />
         <span>编号: </span>
     </a-row>
 </template>
@@ -41,7 +41,7 @@ const nameOptions = ref<OptionData[]>([])
 const codeOptions = ref<OptionData[]>([])
 
 
-const getOptions = (datas: Data[]) => {
+const getOptions = (datas: Data[],searchText:string) => {
     const _codeOptions: OptionData[] = []
     const _nameOptions: OptionData[] = []
     for (let i in datas) {
@@ -54,5 +54,12 @@ const getOptions = (datas: Data[]) => {
         codeOptions.value = _codeOptions
     }
 }
-getOptions(props.datas)
+// getOptions(props.datas,'')
+
+
+const onSearch = (searchText: string) => {
+    console.log(searchText);
+
+
+}
 </script>
