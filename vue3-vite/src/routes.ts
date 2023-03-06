250f2b3db9dc73f1162d912a2ea6cb05e9f4aaf5
pczheng <zhengpc@geowit.cn>
2023-03-06 23:21:49 +0800

    添加异步加载组件的方法

--- vue3-vite/src/routes.ts
+++ vue3-vite/src/routes.ts
@@ -5,7 +5,7 @@ import RelationP from "./components/RelationP.vue";
 
 export const routes = [
   { path: "/", component: HelloWorld },
-  { path: "/table", component: Table },
+  { path: "/table", component: ()=> import("./components/Table.vue")},//异步加载组件
   { path: "/table2", component: Table2 },
   { path: "/relation", component: RelationP },
 ];
